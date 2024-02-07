bc61966ec88100e7acc717d9a6e6419c9b0ffb00
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-02-07 23:22:33 +0530

    feat: added global error handler in backend

--- backend-nodejs-mysql/app.js
+++ backend-nodejs-mysql/app.js
@@ -16,6 +16,12 @@ app.use(verifyToken)
 app.use('/users', userRouter);
 app.use('/accounts', accountRouter);
 
+// Global error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 db.sequelize.sync().then(() => {
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
