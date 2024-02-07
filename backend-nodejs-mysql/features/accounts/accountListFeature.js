c155e838eb01550de21608838f823aede02e4910
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-02-07 15:15:33 +0530

    fix: table names in features

--- backend-nodejs-mysql/features/accounts/accountListFeature.js
+++ backend-nodejs-mysql/features/accounts/accountListFeature.js
@@ -2,7 +2,7 @@ const db = require('../../database');
 
 module.exports = async function accountListFeature(filters) {
     try {
-        return await db.Account.findAll();
+        return await db.accounts.findAll();
     } catch (error) {
         throw new Error('Error fetching accounts');
     }
--- backend-nodejs-mysql/features/users/userListFeature.js
+++ backend-nodejs-mysql/features/users/userListFeature.js
@@ -2,7 +2,7 @@ const db = require('../../database');
 
 module.exports = async function userListFeature(filters) {
     try {
-        return await db.User.findAll();
+        return await db.users.findAll();
     } catch (error) {
         console.log(error)
         throw new Error('Error fetching users');
