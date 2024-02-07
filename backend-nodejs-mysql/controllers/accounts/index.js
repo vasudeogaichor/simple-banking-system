60c47a62ff69b169b834e3fff0345b4114ef6be0
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-02-07 22:35:41 +0530

    fix: list txns conditions

--- backend-nodejs-mysql/controllers/accounts/index.js
+++ backend-nodejs-mysql/controllers/accounts/index.js
@@ -3,12 +3,13 @@ const { accountCreateFeature } = require("../../features/accounts");
 
 async function accountListController(req, res, next) {
     try {
-        if (!(req.user.userType == 'banker')) {
+        if (req.user.userType == 'customer' && req.user.userId != req.query?.user_id) {
             res.status(401).json({ Error: 'Invalid access' })
             return;
+        } else {
+            const result = await accountListFeature(req.query)
+            res.status(200).json(result);
         }
-        const result = await accountListFeature(req.query)
-        res.status(200).json(result);
     } catch (error) {
         console.error(error);
         res.status(400).json({ Error: error.message });
@@ -18,7 +19,7 @@ async function accountListController(req, res, next) {
 async function accountCreateController(req, res, next) {
     try {
         if (req.user.userId !== req.body.user_id) {
-            res.status(401).json(`Error: Invalid access`)
+            res.status(401).json({ Error: 'Invalid access' })
             return;
         }
         const result = await accountCreateFeature(req.body)
